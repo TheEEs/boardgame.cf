0c132eb14c0f339f28d6d288cc687c90592a7b25
TranDat <visualbasic2013@hotmail.com>
2019-07-22 14:19:06 +0700

    Fix postgresql order_by distint query

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,13 +10,12 @@ class GamesController < ApplicationController
     @selected_tags = Tag.find(tags_params) rescue nil
     @tags = Tag.all.shuffle
     near_users = []
+    @games = Game.all.select('"games"."user_id" AS uid', '"games".*')
     if filter_near_me && user_signed_in? 
       near_users_records = User.near([current_user.latitude, current_user.longitude], 20)
       begin 
         near_users += near_users_records.map{|user| [user.id, user.distance]}
       end if near_users_records.any?
-    else
-      @games = Game.all 
     end
 
     unless filter_city_name.blank?
@@ -30,14 +29,15 @@ class GamesController < ApplicationController
     if near_users.any?
       when_clauses = near_users.map do |user| 
         #byebug
-        "WHEN \"games\".\"user_id\" = #{user.first} THEN #{user.last}" 
+        "WHEN uid = #{user.first} THEN #{user.last}" 
       end
       if near_users.any?
-        final_order_query = <<~case
+        final_order_query = %Q=
           (CASE
             #{when_clauses.join(' ')}
           END)
-        case
+        =
+        final_order_query= final_order_query.strip
       else
         final_order_query = ""
       end
