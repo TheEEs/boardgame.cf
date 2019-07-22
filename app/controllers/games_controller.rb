c2c07ff737a5ce90bdade53f6b815d3e1a49d214
TranDat <visualbasic2013@hotmail.com>
2019-07-22 14:32:59 +0700

    add some change

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,7 +10,7 @@ class GamesController < ApplicationController
     @selected_tags = Tag.find(tags_params) rescue nil
     @tags = Tag.all.shuffle
     near_users = []
-    @games = Game.all.select('"games"."user_id" AS uid', '"games".*')
+    @games = Game.all.select('"games"."user_id" AS UID', '"games".*')
     if filter_near_me && user_signed_in? 
       near_users_records = User.near([current_user.latitude, current_user.longitude], 20)
       begin 
@@ -29,7 +29,7 @@ class GamesController < ApplicationController
     if near_users.any?
       when_clauses = near_users.map do |user| 
         #byebug
-        "WHEN uid = #{user.first} THEN #{user.last}" 
+        "WHEN UID = #{user.first} THEN #{user.last}" 
       end
       if near_users.any?
         final_order_query = %Q=
